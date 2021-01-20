60ea255dee2feb50d1f7e20e2a5a7952b63d1be6
Mateusz <mmoczydlowski4@gmail.com>
2021-01-20 02:21:48 +0100

    Make huge code improvements
    Add jsonfile reader; Add node-json-db to store data in; Create automatic timed messages based on users activity; Add test channel to config;

--- common.js
+++ common.js
@@ -1,5 +1,26 @@
-module.exports = {
+'esversion: 6';
+
+var common = {
         randomValue: function (array) {
                 return array[Math.floor(Math.random() * array.length)];
+        },
+        loopAction: function (time, callback) {
+                setTimeout(() => {
+                        callback();
+                        this.loopAction(time, callback);
+                }, time);
+        }
+}
+
+export default class Common {
+        static randomValue (array) {
+                return array[Math.floor(Math.random() * array.length)];
+        }
+
+        static loopAction (time, callback) {
+                setTimeout(() => {
+                        callback();
+                        this.loopAction(time, callback);
+                }, time);
         }
 }
--- index.js
+++ index.js
@@ -1,94 +1,155 @@
-const Discord = require('discord.js');
+'esversion: 6';
+
+import Discord from 'discord.js';
+import Common from './common.js';
+import config from './config.js';
+import { JsonDB } from 'node-json-db';
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig.js';
+import jsonfile from "jsonfile";
+
 const client = new Discord.Client();
-const common = new require('./common.js');
-const config = new require('./config.js');
 const prefix = config.prefix;
-const log = require('log-to-file');
+const db = new JsonDB(new Config("barvilDB", true, false, '/'));
+
+var guild;
+var channels;
+var lastMessageTime;
+
+try {
+	lastMessageTime = new Date(db.getData("time/lastmessagetime"));
+} catch(error) {
+	lastMessageTime = new Date();
+};
 
 // Initial setup info
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log("Setted prefix: " + prefix);
+	console.log("Setted prefix: " + prefix);
+	
+	guild = client.guilds.cache.get(config.guildId);
+	if (!guild) return console.log("Couldn't get the guild.");
+	else console.log("Guild found: " + guild.name);
+
+	channels = {
+		announcements: guild.channels.cache.get(config.channelIds.announcements),
+		general: guild.channels.cache.get(config.channelIds.general),
+		test: guild.channels.cache.get(config.channelIds.test)
+	};
+
+	if (!channels.announcements) return console.log("Couldn't get the announcement channel.");
+	else console.log("Announcement channel found: " + channels.announcements.name);
+
+	if (!channels.announcements) return console.log("Couldn't get the general channel.");
+	else console.log("Announcement channel found: " + channels.general.name);
+
+	if (!channels.announcements) return console.log("Couldn't get the general channel.");
+	else console.log("Test channel found: " + channels.test.name);
+
+});
+
+// Automatic timed messages
+client.on('ready', () => {
+
+	Common.loopAction(5000, () => {
+		let currentTime = new Date();
+
+		if (currentTime - lastMessageTime > "72000000") {
+			message = Common.randomValue(config.longInactivityAnswers);
+			channels.general.send(message);
+		}
+	});
+
+});
+
+
+// Update last message time 
+client.on("message", msg => {
+	if (msg.content.startsWith(prefix) || msg.author.bot) return;
+
+	lastMessageTime = new Date();
+	db.push("time/lastmessagetime", lastMessageTime);
+	console.log("⭕ Zaktualizowano datę wysłania ostatniej wiadomości na " + new Date());
 });
 
 // Commands
 client.on('message', msg => {
-        if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-        const args = msg.content.slice(prefix.length).trim().split(' ');
-        const command = args.shift().toLowerCase();
-        
-        if (!msg.member.hasPermission("ADMINISTRATOR")) {
-                msg.reply("sorry, ale słucham tylko poleceń admina.");
-                return 0;
-        }
+	const args = msg.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+	
+	if (!msg.member.hasPermission("ADMINISTRATOR")) {
+		msg.reply("sorry, ale słucham tylko poleceń admina.");
+		return 0;
+	}
 });
 
 // Automatic responses
 client.on('message', msg => {
-        // Prevent reacting to own messages
-        if(msg.author.bot) return;
+	// Prevent reacting to own messages
+	if(msg.author.bot) return;
 
-        // Prevent null message
-        if (msg.member === null) return;
+	// Prevent null message
+	if (msg.member === null) return;
 
-        // Initial variables
-        const guild = msg.guild;
-        const channels = {
-                announcements: guild.channels.cache.get(config.channelIds.announcements),
-                general: guild.channels.cache.get(config.channelIds.general)
-        }
-        const messageWords = msg.content.split(' ');
-        const senderId = msg.member.user.id;
-        const isAdmin = (msg.member.hasPermission("ADMINISTRATOR"));
-        
-        // Swearing reaction
-        messageWords.forEach((element, index, array) => {
-                if (config.curses.indexOf(element.toLowerCase()) !== -1) {
-                        let answerMessage;
+	// Initial variables
+	const messageWords = msg.content.split(' ');
+	const senderId = msg.member.user.id;
+	const isAdmin = (msg.member.hasPermission("ADMINISTRATOR"));
+	
+	// Swearing reaction
+	messageWords.forEach((element, index, array) => {
+		if (config.curses.indexOf(element.toLowerCase()) !== -1) {
+			let answerMessage;
 
-                        if (!isAdmin) {
-                                answerMessage = common.randomValue(config.curseAnswers);
-                        }
-                        else {
-                                answerMessage = common.randomValue(config.adminCurseAnswers).replace("#ping", "<@" + config.pingUserId + ">");
-                        }
+			if (!isAdmin) {
+				answerMessage = Common.randomValue(config.curseAnswers);
+			}
+			else {
+				answerMessage = Common.randomValue(config.adminCurseAnswers).replace("#ping", "<@" + config.pingUserId + ">");
+			}
 
-                        msg.channel.send(answerMessage);
-                }
-        });
+			msg.channel.send(answerMessage);
+		}
+	});
 
-        // Mention reaction
-        if (msg.mentions.has(client.user)) {
-                if (!isAdmin) {
-                        let answerMessage = common.randomValue(config.mentionAnswers);
-                        msg.reply(answerMessage);
-                }
-                else {
-                        msg.reply("możemy porozmawiać na tickecie?");
-                        setTimeout(() => {
-                                msg.channel.send("To ja otworzę...");
-                                
-                        }, 1500)
-                }
-        }
+	// Mention reaction
+	if (msg.mentions.has(client.user)) {
+		if (!isAdmin) {
+			let answerMessage = Common.randomValue(config.mentionAnswers);
+			msg.reply(answerMessage);
+		}
+		else {
+			msg.reply("możemy porozmawiać na tickecie?");
+			setTimeout(() => {
+				msg.channel.send("To ja otworzę...");
+				
+			}, 1500)
+		}
+	}
 
-        // Announcement reaction
-        if (guild && msg.channel == channels.announcements) {
-                answerMessage = common.randomValue(config.announcementsAnswers).replace("#ping", "<@" + senderId + ">");
-                channels.general.send(answerMessage);
-        }
+	// Announcement reaction
+	if (guild && msg.channel == channels.announcements) {
+		answerMessage = Common.randomValue(config.announcementsAnswers).replace("#ping", "<@" + senderId + ">");
+		channels.general.send(answerMessage);
+	}
 });
 
 client.on('error', error => {
-        console.error('There was an error:', error);
+	console.error('There was an error:', error);
 });
 
-if (process.env.BOT_TOKEN) {
-        client.login(process.env.BOT_TOKEN);
-}
-else {
-        const tokens = new require('./token.js');
-        const token = tokens.token;
-        client.login(token);
-}
+/**
+ * Token should be contained in './token.json'
+ * file as normal string, if there is not such
+ * file app defaults to heroku global BOT_TOKEN
+ * config var
+ */
+jsonfile.readFile('./token.json', function (err, obj) {
+  if (err)
+		client.login(process.env.BOT_TOKEN);
+  else
+		client.login(obj);
+});
+
+
